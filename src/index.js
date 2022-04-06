e5fc70fb4bc9da5d795e42b55f305b4656f8cc27
trevorw1990 <95665741+trevorw1990@users.noreply.github.com>
2022-04-05 22:48:25 -0400

    crypto update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+import App from "./App";
+//IMPORT BrowserRouter and rename it to Router
+import { BrowserRouter as Router } from "react-router-dom";
 
+//Wrap the App Component with the Router component to enable the router features
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
